6769e35640d09c3353de399aad81b6a7003a3b19
BrunovDaniil;) <danilabrunov@gmail.com>
2022-07-02 00:47:04 +0300

    CScourse2022GAZ
    CScourse2022GAZ

---
+++ CScourse2022GAZ/Lab 1/Ex2/Starter/Laba1Ex2/Laba1Ex2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laba1Ex2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void testbuttom_Click(object sender, RoutedEventArgs e)
+        {
+            string line = testinput.Text;
+            line = line.Replace(",", " y:");
+            line = "x:" + line;
+            formattedText.Text = line;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                line = line.Replace(",", " y:");
+                line = "x:" + line + "\n";
+                formattedText.Text += line;
+            } 
+        }
+    }
+}
---
+++ CScourse2022GAZ/Lab 6/Ex1/Starter/StressTest/StressTestTypes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StressTest
+{
+    // Add the Material, CrossSection and TestResult enumerations.
+    public enum Material
+    {
+        StainlessSteel,
+        Aluminum,
+        ReinforcedConcrete,
+        Composite,
+        Titanium
+    }
+    public enum CrossSection
+    {
+        IBeam,
+        Box,
+        ZShaped,
+        CShaped
+    }
+    public enum TestResult
+    {
+        Pass,
+        Fail
+    }
+}
